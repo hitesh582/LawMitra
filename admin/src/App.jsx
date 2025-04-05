24ad7c495ebe8e87d5a7cbec2a5970c8eee9b01c
Hitesh Kumar <hiteshkv582@gmail.com>
2025-04-06 00:53:50 +0530

    removed unnecessary routes from admin panel add list order

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
-import Add from "./pages/Add";
-import List from "./pages/List";
-import Orders from "./pages/Orders";
 import Login from "./components/Login";
 import LawyerVerifications from "./pages/LawyerVerifications.jsx";
 import LawyerDetails from "./pages/LawyerDetails.jsx";
@@ -34,9 +31,6 @@ const App = () => {
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add token={token} />} />
-                <Route path="/list" element={<List token={token} />} />
-                <Route path="/orders" element={<Orders token={token} />} />
                 <Route path="/verifications" element={<LawyerVerifications token={token} />} />
                 <Route path="/verifications/:id" element={<LawyerDetails token={token} />} />
                 <Route path="/approved-lawyers" element={<ApprovedLawyers token={token} />} />
